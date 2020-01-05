0411e8ec505d6e9683ceb7a0e557e2134289d114
khoa0319 <khoa0319@gmail.com>
2020-01-05 14:07:44 +0700

    update bootstrapping files

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_bak             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+files=".bashrc .vimrc .vim .zshrc .oh-my-zsh"    # list of files/folders to symlink in homedir
 
 ##########
 
@@ -20,9 +20,9 @@ echo "done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
 	echo "Moving any existing dotfiles from ~ to $olddir"
-	mv ~/.$file ~/dotfiles_old/
+	mv ~/$file ~/$olddir/$file
 	echo "Creating symlink to $file in home directory."
-	ln -s $dir/$file ~/.$file
+	ln -s $dir/$file ~/$file
 done
 
 install_zsh () {
