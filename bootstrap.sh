6dfb3c1ad043dc0f0b8d00b9fe877ebe4b0e2b7d
khoahuynhdev <khoa0319@gmail.com>
2020-09-14 15:10:25 +0700

    update setup script

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ set -e # abort on error
 function install() {
   if [[ $reply =~ ^[Yy]$ ]]; then
     if [[ $(uname -s) == "Darwin" ]]; then
-      bash "$ROOT/osx/setup-osx.sh"
+      ./osx/setup-osx.sh
     elif[[ $(uname -s) == "Linux" ]]
       "$ROOT/setup-linux.sh"
     fi
