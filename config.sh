305cc2e613b6708a86154be69665879827830b21
khoahuynhdev <khoa0319@gmail.com>
2020-09-16 10:35:15 +0700

    add common files to symlink

---
+++ config.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+source_dir="$HOME/dotfiles"
+
+function symlink() {
+  source=$1
+  target=$2
+  if [[ -d $2 ]]; then
+    echo $target is a directory
+    echo backup $target
+    mv $target "$target-backup"
+  fi
+  echo "symlink $source to $target"
+  ln -sf $source $target
+}
+
+install_spaceship_theme() {
+  echo Install Spaceship theme!
+  git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+  ln -sf "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+}
+
+
+for file in $(ls "$source_dir/dot"); do
+  symlink $source_dir/$file $HOME/.$file
+done
+
+# symlink config folder
+config_dir=$HOME/.config
+target_dirs="alacritty nvim"
+for dir in $target_dirs; do
+  symlink "$source_dir/$dir" $config_dir/$dir
+done
+
+source $HOME/.zshrc
+install_spaceship_theme
