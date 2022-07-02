bcb7fc44a08e36ae10bad48dac98d73a6163377d
khoahuynhdev <khoa0319@gmail.com>
2022-07-02 16:34:39 +0700

    telescope key map enhancement

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -1,6 +1,17 @@
+local default_opts = { noremap = true, silent = true }
+vim.g.mapleader = ','
 -- Quickly goback
 vim.keymap.set("n", "<BS>", "<C-o>")
 
 
 -- nvim-tree
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>")
+
+-- telescope
+-- find file with telescope
+vim.keymap.set("n", "<C-p>", ":Telescope find_files<CR>")
+-- fuzzy search git file, respect .gitignore
+vim.keymap.set("n", "<Leader>fg", ":lua require('telescope.builtin').git_files()<CR>", default_opts)
+-- find which in buffer
+vim.keymap.set("n", "<Leader>bf", ":Telescope buffers<CR>", default_opts)
+
