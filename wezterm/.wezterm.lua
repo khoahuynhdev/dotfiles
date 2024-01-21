5260463e24d7af9c246e1f196a6d045349df1c35
daniel.huynh <daniel.huynh@shopback.com>
2024-01-21 21:46:06 +0700

    feat: update wezterm
    Signed-off-by: daniel.huynh <daniel.huynh@shopback.com>

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,13 +7,21 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
+config.enable_tab_bar = false
+config.color_scheme = "Dracula (Official)"
+config.font = wezterm.font("FiraCode Nerd Font")
+config.font_size = 15
 
--- For example, changing the color scheme:
-config.color_scheme = 'Dracula (Official)'
+config.keys = {
+	{
+		key = "f",
+		mods = "CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
 
 -- and finally, return the configuration to wezterm
 return config
