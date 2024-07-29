f7771aa7c825a8faf858f6650f46a9c37d2a538f
khoahuynhdev <khoa.hd96@gmail.com>
2024-07-29 13:35:46 +0000

    chore: update success msg

--- install-zsh-theme-spaceship.sh
+++ install-zsh-theme-spaceship.sh
@@ -8,4 +8,4 @@ git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/
 echo "symlink..."
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-echo "success íº€"
+echo "success!"
