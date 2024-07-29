458f3ddf132a47c2bae9912f844d8e3d505d2908
khoahuynhdev <khoa.hd96@gmail.com>
2024-07-29 15:50:26 +0000

    chore: update install theme script

--- install-zsh-theme-spaceship.sh
+++ install-zsh-theme-spaceship.sh
@@ -3,9 +3,9 @@
 set -x
 
 echo "cloning spaceship promp..."
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
 
 echo "symlink..."
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+ln -sf "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
 
 echo "success!"
