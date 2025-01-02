ce880faac0bcfe106fc246f5e88a1fef92553394
khoahd-anz <huynhdang.khoa@anz.com>
2025-01-02 16:06:01 +0700

    feat: update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -11,7 +11,7 @@ if wezterm.config_builder then
 end
 
 config.enable_tab_bar = false
-config.color_scheme = "OneDark (base16)"
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("FiraCode Nerd Font")
 config.font_size = 15
 
@@ -23,5 +23,69 @@ config.keys = {
 	},
 }
 
+-- Window setting/ appearance
+-- config.window_decorations = "RESIZE"
+
+config.window_padding = {
+	left = "1cell",
+	right = "1cell",
+	top = "0.0cell",
+	bottom = "0.5cell",
+}
+
+config.initial_rows = 42
+config.initial_cols = 124
+
+config.enable_scroll_bar = true
+config.scrollback_lines = 5000
+config.default_workspace = "main"
+
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+	saturation = 0.24,
+	brightness = 0.5,
+}
+
+-- Tab bar
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = false
+config.status_update_interval = 1000
+wezterm.on("update-status", function(window, pane)
+	local basename = function(s)
+		return string.gsub(s, "(.*[/\\])(.*)", "%2")
+	end
+
+	-- Current working directory
+	local cwd = pane:get_current_working_dir()
+	if cwd then
+		if type(cwd) == "userdata" then
+			cwd = basename(cwd.file_path)
+		else
+			cwd = basename(cwd)
+		end
+	else
+		cwd = ""
+	end
+
+	-- Current command
+	local cmd = pane:get_foreground_process_name()
+	cmd = cmd and basename(cmd) or ""
+
+	-- Time
+	local time = wezterm.strftime("%H:%M")
+
+	-- Right status
+	window:set_right_status(wezterm.format({
+		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+		{ Text = " | " },
+		{ Foreground = { Color = "#e0af68" } },
+		{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+		"ResetAttributes",
+		{ Text = " | " },
+		{ Text = wezterm.nerdfonts.md_clock .. "  " .. time },
+		{ Text = "  " },
+	}))
+end)
 -- and finally, return the configuration to wezterm
 return config
