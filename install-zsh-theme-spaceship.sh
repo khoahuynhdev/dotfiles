4ca8548829d7080a5e1686f4c74b518392e779ea
daniel <khoa.hd96@gmail.com>
2025-06-11 18:25:16 +0700

    feat: continue

--- install-zsh-theme-spaceship.sh
+++ install-zsh-theme-spaceship.sh
@@ -3,9 +3,9 @@
 set -x
 
 echo "cloning spaceship promp..."
-git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt" --depth=1
 
 echo "symlink..."
-ln -sf "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
+ln -sf "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
 
 echo "success!"
