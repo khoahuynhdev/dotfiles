9e0cc191d7a64aa0a2044468db8f1ca4986ffe36
daniel <khoa.hd96@gmail.com>
2025-06-18 00:27:51 +0700

    fix: install docker script

--- install-docker.sh
+++ install-docker.sh
@@ -1,11 +1,11 @@
-#!/usr/env bash
+#!/usr/bin/env bash
 
 set -e
 
 sudo apt update && sudo apt upgrade
 sudo apt install --no-install-recommends apt-transport-https ca-certificates curl gnupg2
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
 sudo apt install docker-compose-plugin
